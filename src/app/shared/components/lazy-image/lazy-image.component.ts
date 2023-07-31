8df90bf36e7041fa0a6386df1462c2ef9b39ff4b
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-07-31 02:53:27 -0600

    animate css

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.scss'],
 })
-export class LazyImageComponent {}
+export class LazyImageComponent implements OnInit {
+  @Input({ required: true }) urlImage!: string;
+  @Input({ required: true }) alt: string = '';
+  hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.urlImage) throw new Error('ImageURL is required.');
+  }
+
+  onLoad(): void {
+    this.hasLoaded = true;
+  }
+}
