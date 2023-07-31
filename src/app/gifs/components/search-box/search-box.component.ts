123c069055741e3b200c92ac7a9f4fdc97520d78
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-07-30 23:39:38 -0600

    add services

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss'],
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput') tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private readonly gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private readonly gifsService: GifsService) {}
+  get tags() {
+    return this.gifsService.tagsHistory;
+  }
+}
