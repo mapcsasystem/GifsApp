af00660d005ae3cb103a40a6da29c71ce948db99
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-07-31 00:47:48 -0600

    add api key

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
-
-  constructor() {}
+  private apiKey = 'S5Mtb4Yh20q2PnJVtRtCaFngiWudPInE';
+  private serviceUrl = 'https://api.giphy.com/v1/gifs';
+  gifList: Gif[] = [];
+  constructor(private readonly http: HttpClient) {}
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
   private organizeHistory(tag: string): void {
-    tag = tag.toLocaleLowerCase();
+    tag = tag.toLocaleLowerCase().trim();
     if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
@@ -24,5 +28,14 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
   }
 }
